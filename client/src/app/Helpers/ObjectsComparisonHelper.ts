5b0366201e5aa0e14100a406c0c31e5899294ad0
AnonFilipchuk <anton.filipchukq12@gmail.com>
2023-04-25 09:22:33 +0000

    Before comparison refactor

--- client/src/app/Helpers/ObjectsComparisonHelper.ts
+++ client/src/app/Helpers/ObjectsComparisonHelper.ts
@@ -3,7 +3,15 @@ export class ObjectsComparisonHelper {
         return JSON.stringify(firstObject) == JSON.stringify(secondObject);
     }
 
-    public ifArrayHasAnObject(array: any[], obj: any) {
+    public ifTwoObjectsAreEqualByProperty(firstObject: any, secondObject: any, property: any): boolean {
+        return firstObject.property === secondObject.property;
+    }
+
+    public ifArrayHasAnObject(array: any[], obj: any): boolean {
         return array.filter(o => this.ifTwoObjectsAreEqual(o, obj)).length > 0;
+    }
+
+    public ifArrayHasAnObjectByProperty(array: any[], obj: any, property: any): boolean {
+        return array.filter(o => this.ifTwoObjectsAreEqualByProperty(o, obj, property)).length > 0;
     }
 }
